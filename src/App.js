ed2494432ac8a388187ada61674d516dd1a468ed
maguiceri <magui.cerisola@gmail.com>
2020-07-08 01:16:20 -0700

    clean

--- src/App.js
+++ src/App.js
@@ -6,29 +6,7 @@ class App extends React.Component{
 
     render(){
 
-        const items = [
-            {
-                img:"https://courseit.com.ar/cursos/logo.png",
-                title:"primer producto",
-                price: 1000
-            },
-            {
-                img:"https://courseit.com.ar/cursos/logo.png",
-                title:"segundo producto",
-                price:8
-            },
-            {
-                img:"https://courseit.com.ar/cursos/logo.png",
-                title:"tercer producto",
-                price:30 
-            },
-        
-            {
-                img:"https://courseit.com.ar/cursos/logo.png",
-                title:"cuarto producto",
-                price:9
-            }
-        ]
+ 
         return(
             <div>
             <Carousel title="carusel 1" items={items} />
